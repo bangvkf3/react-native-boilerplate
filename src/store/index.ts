ec85fc3bba010b36721a6e2c7d58412cae4f8b56
JunHyeok.Bang <bangvkf3@gmail.com>
2021-06-07 21:15:54 +0900

    feat : add a redux-thunk

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
 import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 import { logger } from "./logger";
 import rootReducer from "./rootReducer";
 
-let middlewares: any[] = [];
+let middlewares: any[] = [thunk];
 if (__DEV__) {
   middlewares.push(logger);
 }
--- src/store/login/actions.ts
+++ src/store/login/actions.ts
@@ -1,9 +1,9 @@
 import { LoginAction, LogoutAction, User } from "./types";
 
 export const loginAction = (loggedUser: User): LoginAction => ({
-  type: "login",
+  type: "@login/login",
   loggedUser,
 });
 export const logoutAction = (): LogoutAction => ({
-  type: "logout",
+  type: "@login/logout",
 });
